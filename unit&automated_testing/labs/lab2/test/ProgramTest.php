a82dbff7f8e567128db4e8ea33bfbbe2d0761209
Adham-Ahmed <70696055+Adham-Ahmed@users.noreply.github.com>
2022-03-28 18:11:37 +0200

    Add files via upload

--- unit&automated_testing/labs/lab2/test/ProgramTest.php
+++ unit&automated_testing/labs/lab2/test/ProgramTest.php
@@ -2,30 +2,30 @@
 use App\User;
 class ProgramTest extends PHPUnit\Framework\TestCase {
 
-    public $user;
+    public static $user;
 
-    public function __construct()
-    {
-       $this->user =new User();
+    // public function __construct()
+    // {
+    //    $this->user =new User();
         
-    }
-    // public static function  setUpBeforeClass():void{
-    //     self::$user=new User();
     // }
+    public static function  setUpBeforeClass():void{
+        self::$user=new User();
+    }
     public function testTrue_nameGetter(){
-        return $this->assertTrue($this->user->name()=="adham");
+        return $this->assertTrue(self::$user->name()=="adham");
     }
 
     public function testTrue_nameSetter(){
-        return $this->assertTrue($this->user->name('amal')=='amal');
+        return $this->assertTrue(self::$user->name('amal')=='amal');
     }
 
     public function testTrue_emailGetter(){
-        return $this->assertTrue($this->user->email()=='adham@gmail.com');
+        return $this->assertTrue(self::$user->email()=='adham@gmail.com');
     }
 
     public function testTrue_email(){
-        return $this->assertTrue($this->user->email('amal@yahoo.com')=='amal@yahoo.com');
+        return $this->assertTrue(self::$user->email('amal@yahoo.com')=='amal@yahoo.com');
     }
 
     
